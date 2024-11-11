e5c371ff3fd93122b92d58b424a1a51b22a712f9
cream-cmyk <cream921@163.com>
2024-11-11 14:59:01 +0800

    月度账单-切换弹框

--- src/Pages/Month/index.js
+++ src/Pages/Month/index.js
@@ -1,6 +1,60 @@
+import { NavBar, DatePicker } from 'antd-mobile'
+import classNames from 'classnames'
+import { useState } from 'react'
+import './index.scss'
+
 const Month = () => {
+  const [dateVisible, setDateVisible] = useState(false)
+  const onConfirmFun = ()=>{
+    setDateVisible(false)
+  }
   return (
-    <div>这是Month二级路由</div>
+    <div className="monthlyBill">
+      <NavBar className="nav" backArrow={false}>
+        月度收支
+      </NavBar>
+      <div className="content">
+        <div className="header">
+          {/* 时间切换区域 */}
+          <div className="date" onClick={() => setDateVisible(true)}>
+            <span className="text">
+              2023 | 3月账单
+            </span>
+            {/*expand存在箭头朝下，否则朝上，所以用className通过dateVisible状态值来判断expand是否存在*/}
+            <span className={classNames('arrow', dateVisible && 'expand')}></span>
+          </div>
+          {/* 统计区域 */}
+          <div className='twoLineOverview'>
+            <div className="item">
+              <span className="money">{100}</span>
+              <span className="type">支出</span>
+            </div>
+            <div className="item">
+              <span className="money">{200}</span>
+              <span className="type">收入</span>
+            </div>
+            <div className="item">
+              <span className="money">{200}</span>
+              <span className="type">结余</span>
+            </div>
+          </div>
+          {/* 时间选择器 */}
+          <DatePicker
+            className="kaDate"
+            title="记账日期"
+            precision="month"
+            visible={dateVisible}
+            onCancel={() => setDateVisible(false)}
+            //因为点击确定还有别的功能需要实现，所以把“关闭弹窗”功能放在回调函数中实现
+            onConfirm={() => onConfirmFun}
+            //蒙层区域
+            onClose={()=>setDateVisible(false)}
+            max={new Date()}
+          />
+        </div>
+      </div>
+    </div >
   )
 }
+
 export default Month
