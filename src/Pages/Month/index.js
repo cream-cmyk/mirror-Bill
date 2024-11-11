823b308f0095e7ab4801915aec5bdb1c19049859
cream-cmyk <cream921@163.com>
2024-11-11 15:28:07 +0800

    月度账单-切换时间显示

--- src/Pages/Month/index.js
+++ src/Pages/Month/index.js
@@ -1,13 +1,21 @@
 import { NavBar, DatePicker } from 'antd-mobile'
 import classNames from 'classnames'
+import dayjs from 'dayjs'
 import { useState } from 'react'
 import './index.scss'
 
 const Month = () => {
+  //选择框弹出状态--默认false
   const [dateVisible, setDateVisible] = useState(false)
-  const onConfirmFun = ()=>{
+  //点击确认，调用函数
+  const onConfirmFun = (date) => {
     setDateVisible(false)
+    console.log(date)
+    setCurrentDate(dayjs(date).format('YYYY | MM'))
   }
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs(new Date()).format('YYYY | MM')
+  })
   return (
     <div className="monthlyBill">
       <NavBar className="nav" backArrow={false}>
@@ -18,7 +26,7 @@ const Month = () => {
           {/* 时间切换区域 */}
           <div className="date" onClick={() => setDateVisible(true)}>
             <span className="text">
-              2023 | 3月账单
+              {currentDate + ''}月账单
             </span>
             {/*expand存在箭头朝下，否则朝上，所以用className通过dateVisible状态值来判断expand是否存在*/}
             <span className={classNames('arrow', dateVisible && 'expand')}></span>
@@ -46,9 +54,9 @@ const Month = () => {
             visible={dateVisible}
             onCancel={() => setDateVisible(false)}
             //因为点击确定还有别的功能需要实现，所以把“关闭弹窗”功能放在回调函数中实现
-            onConfirm={() => onConfirmFun}
+            onConfirm={onConfirmFun}
             //蒙层区域
-            onClose={()=>setDateVisible(false)}
+            onClose={() => setDateVisible(false)}
             max={new Date()}
           />
         </div>
