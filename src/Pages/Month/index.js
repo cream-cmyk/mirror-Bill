18f8aff6185ec3c890a823c1da831edb983e4d11
cream-cmyk <cream921@163.com>
2024-11-12 10:30:04 +0800

    基于当前月份的数据统计计算

--- src/Pages/Month/index.js
+++ src/Pages/Month/index.js
@@ -1,21 +1,58 @@
 import { NavBar, DatePicker } from 'antd-mobile'
 import classNames from 'classnames'
 import dayjs from 'dayjs'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
+import { useSelector } from 'react-redux'
 import './index.scss'
+import _ from 'lodash'
 
 const Month = () => {
+  //按月做分组处理   1.从Redux拿到数据
+  const billList = useSelector(state => state.bill.billList)
+  //2.数据做二次处理useMemo
+  const monthGroup = useMemo(() => {
+    //返回计算后的值  3.按月分组 lodash
+    return _.groupBy(billList, (item) => dayjs(item.date).format("YYYY | MM"))
+  }, [billList])
+  console.log(monthGroup)
+
   //选择框弹出状态--默认false
   const [dateVisible, setDateVisible] = useState(false)
-  //点击确认，调用函数
-  const onConfirmFun = (date) => {
-    setDateVisible(false)
-    console.log(date)
-    setCurrentDate(dayjs(date).format('YYYY | MM'))
-  }
+  //修改日期状态
   const [currentDate, setCurrentDate] = useState(() => {
     return dayjs(new Date()).format('YYYY | MM')
   })
+  //修改当前获取月度列表
+  const [currentMonthList, setCurrentMonthList] = useState([])
+
+  //点击确认，调用函数
+  const onConfirmFun = (date) => {
+    //切换弹框状态
+    setDateVisible(false)
+    console.log(date)
+    //格式化回调日期
+    const formatDate = dayjs(date).format('YYYY | MM')
+    //修改日期view
+    setCurrentDate(formatDate)
+    // 根据获取到的时间作为key取当月的账单数组
+    setCurrentMonthList(monthGroup[formatDate])
+  }
+
+  //计算统计
+  const viewAll = useMemo(() => {
+    //收入 currentMonthList做过滤
+    const income = currentMonthList.filter((item) => item.type === "income")
+      //对于数组中的每个对象 c，回调函数计算 c.money ,然后将这个值加到累积器 a 上
+      .reduce((a, c) => a + c.money, 0)
+    //支出
+    const pay = currentMonthList.filter((item) => item.type === "pay")
+      .reduce((a, c) => a + c.money, 0)
+    return {
+      income, pay,
+      total: income + pay
+    }
+  }, [currentMonthList])//根据currentMonthList进行变化，所以他作为依赖项传入
+
   return (
     <div className="monthlyBill">
       <NavBar className="nav" backArrow={false}>
@@ -34,15 +71,16 @@ const Month = () => {
           {/* 统计区域 */}
           <div className='twoLineOverview'>
             <div className="item">
-              <span className="money">{100}</span>
+              {/*toFixed(2)保留两位小数*/}
+              <span className="money">{viewAll.pay.toFixed(2)}</span>
               <span className="type">支出</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">{viewAll.income.toFixed(2)}</span>
               <span className="type">收入</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">{viewAll.total.toFixed(2)}</span>
               <span className="type">结余</span>
             </div>
           </div>
